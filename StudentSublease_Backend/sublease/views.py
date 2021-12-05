9ec2e7439634b8c61abe3842d0d5ae9920be0e34
ryantobin77 <ryantobin77@gmail.com>
2021-12-04 19:45:39 -0500

    Added distance in json response

--- StudentSublease_Backend/sublease/views.py
+++ StudentSublease_Backend/sublease/views.py
@@ -36,26 +36,26 @@ def create_listing(request):
                             gender_preference=request.POST['gender_preference'], start_date=start_date, end_date=end_date, rent_per_month=request.POST['rent_per_month'], num_tenants=request.POST['num_tenants'], fees=request.POST['fees'])
         new_listing.amenities.set(amenities)
         new_listing.save()
-        return JsonResponse(new_listing.json_representation(), status=201)
+        json_response_listing = new_listing.json_representation()
+        json_response_listing["distance"] = 0.0
+        return JsonResponse(json_response_listing, status=201)
     else:
         return HttpResponse(status=400)
 
 
 @csrf_exempt
 def search_listings(request):
-    if request.method == "GET":
-        filtered_listings = []
-        if 'lat' in request.GET and 'long' in request.GET:
-            listings = StudentListing.objects.all().order_by('-listed_date')
-            for listing in listings:
-                lat1 = float(request.GET['lat'])
-                long1 = float(request.GET['long'])
-                if utilities.find_distance(lat1=lat1, long1=long1, lat2=listing.address.lat, long2=listing.address.long) <= constants.DEFAULT_DISTANCE_BETWEEN_COLLEGES:
-                    filtered_listings.append(listing)
-        else:
-            listings = StudentListing.objects.all().order_by('-listed_date')
-            filtered_listings = list(listings)
-        listing_results = [listing.json_representation() for listing in filtered_listings]
-        return JsonResponse(listing_results, safe=False, status="200")
+    filtered_listings = []
+    if request.method == "GET" and 'lat' in request.GET and 'long' in request.GET:
+        listings = StudentListing.objects.all().order_by('-listed_date')
+        for listing in listings:
+            lat1 = float(request.GET['lat'])
+            long1 = float(request.GET['long'])
+            distance = utilities.find_distance(lat1=lat1, long1=long1, lat2=listing.address.lat, long2=listing.address.long)
+            if distance <= constants.DEFAULT_DISTANCE_BETWEEN_LOCATIONS:
+                listing_result = listing.json_representation()
+                listing_result["distance"] = round(distance, 2)
+                filtered_listings.append(listing_result)
+        return JsonResponse(filtered_listings, safe=False, status="200")
     else:
-        return HttpResponse(status=400)
+        return JsonResponse(filtered_listings, safe=False, status="400")
